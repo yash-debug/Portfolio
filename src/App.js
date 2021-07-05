fcd57a5ffff4990ae491d1f64cafa079b6c3a002
Yash raj <yashrajsinghc123@gmail.com>
2021-07-05 14:50:55 +0530

    Some unnecessary files were removed.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import Home from './components/Home'
 import About from './components/About'
 import Projects from './components/Projects'
-import Contact from './components/Contact'
 import Navbar from './components/Navbar'
 import './App.css'
 import { Route, Switch, Redirect } from 'react-router-dom'
@@ -15,7 +14,6 @@ const App = () => {
               <Route exact path='/' component={Home} />
               <Route exact path='/about' component={About} />
               <Route exact path='/projects' component={Projects} />
-              <Route exact path='/Contact' component={Contact} />
               <Redirect to="/" />
             </Switch>
             
